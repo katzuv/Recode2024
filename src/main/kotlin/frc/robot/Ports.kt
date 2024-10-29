b081a47c7e95631c96f14b861740e06e3cd3c2e3
Dan Katzuv <dan.dan.kafe@gmail.com>
2024-10-30 01:55:41 +0200

    Fix ktlint comments

--- src/main/kotlin/frc/robot/Ports.kt
+++ src/main/kotlin/frc/robot/Ports.kt
@@ -2,67 +2,72 @@ package frc.robot
 
 class Ports {
     object SwerveDrive {
-        const val FRONT_LEFT_DRIVE_MOTOR_ID: Int = 3
-        const val FRONT_LEFT_ANGLE_MOTOR_ID: Int = 4
-        const val FRONT_RIGHT_DRIVE_MOTOR_ID: Int = 7
-        const val FRONT_RIGHT_ANGLE_MOTOR_ID: Int = 8
-        const val REAR_LEFT_DRIVE_MOTOR_ID: Int = 5
-        const val REAR_LEFT_ANGLE_MOTOR_ID: Int = 2
-        const val REAR_RIGHT_DRIVE_MOTOR_ID: Int = 1
-        const val REAR_RIGHT_ANGLE_MOTOR_ID: Int = 6
+        private const val FRONT_LEFT_DRIVE_MOTOR_ID = 3
+        private const val FRONT_LEFT_ANGLE_MOTOR_ID = 4
+        private const val FRONT_RIGHT_DRIVE_MOTOR_ID = 7
+        private const val FRONT_RIGHT_ANGLE_MOTOR_ID = 8
+        private const val REAR_LEFT_DRIVE_MOTOR_ID = 5
+        private const val REAR_LEFT_ANGLE_MOTOR_ID = 2
+        private const val REAR_RIGHT_DRIVE_MOTOR_ID = 1
+        private const val REAR_RIGHT_ANGLE_MOTOR_ID = 6
 
         val DRIVE_IDS: IntArray =
             intArrayOf(
                 FRONT_LEFT_DRIVE_MOTOR_ID,
                 FRONT_RIGHT_DRIVE_MOTOR_ID,
                 REAR_LEFT_DRIVE_MOTOR_ID,
-                REAR_RIGHT_DRIVE_MOTOR_ID)
+                REAR_RIGHT_DRIVE_MOTOR_ID,
+            )
         val ANGLE_IDS: IntArray =
             intArrayOf(
                 FRONT_LEFT_ANGLE_MOTOR_ID,
                 FRONT_RIGHT_ANGLE_MOTOR_ID,
                 REAR_LEFT_ANGLE_MOTOR_ID,
-                REAR_RIGHT_ANGLE_MOTOR_ID)
+                REAR_RIGHT_ANGLE_MOTOR_ID,
+            )
 
-        const val FRONT_LEFT_ENCODER_ID: Int = 2
-        const val FRONT_RIGHT_ENCODER_ID: Int = 7
-        const val REAR_LEFT_ENCODER_ID: Int = 3
-        const val REAR_RIGHT_ENCODER_ID: Int = 8
+        private const val FRONT_LEFT_ENCODER_ID = 2
+        private const val FRONT_RIGHT_ENCODER_ID = 7
+        private const val REAR_LEFT_ENCODER_ID = 3
+        private const val REAR_RIGHT_ENCODER_ID = 8
 
         val ENCODER_IDS: IntArray =
             intArrayOf(
                 FRONT_LEFT_ENCODER_ID,
                 FRONT_RIGHT_ENCODER_ID,
                 REAR_LEFT_ENCODER_ID,
-                REAR_RIGHT_ENCODER_ID)
+                REAR_RIGHT_ENCODER_ID,
+            )
 
-        var FRONT_LEFT_DRIVE_INVERTED: Boolean = true
-        var FRONT_LEFT_ANGLE_INVERTED: Boolean = true
-        var FRONT_RIGHT_DRIVE_INVERTED: Boolean = true
-        var FRONT_RIGHT_ANGLE_INVERTED: Boolean = true
-        var REAR_LEFT_DRIVE_INVERTED: Boolean = true
-        var REAR_LEFT_ANGLE_INVERTED: Boolean = true
-        var REAR_RIGHT_DRIVE_INVERTED: Boolean = true
+        private const val FRONT_LEFT_DRIVE_INVERTED = true
+        private const val FRONT_LEFT_ANGLE_INVERTED = true
+        private const val FRONT_RIGHT_DRIVE_INVERTED = true
+        private const val FRONT_RIGHT_ANGLE_INVERTED = true
+        private const val REAR_LEFT_DRIVE_INVERTED = true
+        private const val REAR_LEFT_ANGLE_INVERTED = true
+        private const val REAR_RIGHT_DRIVE_INVERTED = true
         val DRIVE_INVERTED: BooleanArray =
             booleanArrayOf(
                 FRONT_LEFT_DRIVE_INVERTED,
                 FRONT_RIGHT_DRIVE_INVERTED,
                 REAR_LEFT_DRIVE_INVERTED,
-                REAR_RIGHT_DRIVE_INVERTED)
-        var REAR_RIGHT_ANGLE_INVERTED: Boolean = true
+                REAR_RIGHT_DRIVE_INVERTED,
+            )
+        private const val REAR_RIGHT_ANGLE_INVERTED = true
         val ANGLE_INVERTED: BooleanArray =
             booleanArrayOf(
                 FRONT_LEFT_ANGLE_INVERTED,
                 FRONT_RIGHT_ANGLE_INVERTED,
                 REAR_LEFT_ANGLE_INVERTED,
-                REAR_RIGHT_ANGLE_INVERTED)
+                REAR_RIGHT_ANGLE_INVERTED,
+            )
     }
 
     object UI {
-        const val JOYSTICK_TRIGGER: Int = 1
-        const val JOYSTICK_TOP_BOTTOM_BUTTON: Int = 2
-        const val JOYSTICK_TOP_LEFT_BUTTON: Int = 3
-        const val JOYSTICK_TOP_RIGHT_BUTTON: Int = 4
-        const val JOYSTICK_RIGHT_BIG_BUTTON: Int = 16
+        const val JOYSTICK_TRIGGER = 1
+        const val JOYSTICK_TOP_BOTTOM_BUTTON = 2
+        const val JOYSTICK_TOP_LEFT_BUTTON = 3
+        const val JOYSTICK_TOP_RIGHT_BUTTON = 4
+        const val JOYSTICK_RIGHT_BIG_BUTTON = 16
     }
 }
